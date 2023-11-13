2ee2ef7d1b643e82693ee9ad42b9ec01bc9bcaa2
differs <keepmysterious@tutanota.com>
2023-11-13 20:57:23 +0800

    .

--- inscribe/src/lib.rs
+++ inscribe/src/lib.rs
@@ -1,17 +1,37 @@
 #![no_std]
 // #![recursion_limit = "256"]
 
+use gstd::{ActorId, ToOwned};
 // use gstd::{exec, msg, prelude::*, ActorId};
-// use inscribe_io::{};
+// use inscribe_io::{Query, Reply, Action, Event, InscribeIoStates};
+use io::{Query, Reply, Action, Event, InscribeIoStates};
+
+
+static mut INSCRIBEIOSTATES: Option<InscribeIoStates> = None;
+
 
 #[no_mangle]
 extern "C" fn init() {
+    unsafe { INSCRIBEIOSTATES = Some(InscribeIoStates::default());
+ };
 
 
 }
 
 #[no_mangle]
 extern "C" fn handle() {
+    let state = unsafe { INSCRIBEIOSTATES.as_mut().expect("failed to get state as mut") };
+    let action: Action = gstd::msg::load().expect("failed to load action");
+
+    match action {
+        Action::Transfer { _inscribe_id, _to, _amount } => {
+            todo!();
+        },
+
+        // Action::Transfer { _inscribe_id, _to, _amount }
+        
+    }
+
 
 
 }
@@ -21,28 +41,22 @@ extern "C" fn handle() {
 #[no_mangle]
 
 extern "C" fn state() {
-    // let query = gstd::msg::load().expect("failed to load query");
+    let query = gstd::msg::load().expect("failed to load query");
     // let mut state: VaraBetsStates = unsafe { VARABETSSTATES.as_ref().expect("failed to get contract state").clone() };
 
-    // let mut GA
-    // let reply = match query {
-        // Query::All => Reply::All(state.clone()),
-        // // Query::Rounds() => Reply::Rounds(state.current_rounds()),
-        // Query::Whoami => Reply::Whoami(gstd::msg::source()), // all zero addr
-        // Query::BlockNumber => Reply::BlockNumber(gstd::exec::block_height()),
-        // Query::BlockTimestamp => Reply::BlockTimestamp(gstd::exec::block_timestamp()),
-        // Query::ProgramId => Reply::ProgramId(gstd::exec::program_id()),
-        // Query::MessageId => Reply::MessageId(gstd::msg::id()),
-        // Query::Title() => Reply::Title(state.last_round().1),
-        // Query::Last() => Reply::Last(state.last_round().0, state.last_round().1),
-        // // Query::BetsRoundState() => Reply::BetsRoundState(unsafe { BETSROUNDSTATE.clone() }),
-        // Query::HashInserted() => Reply::HashInserted(state.inquire_current_card_hash().0, state.inquire_current_card_hash().1),
-        // Query::Beted() => todo!(),
-        // Query::CardsInserted() => Reply::CardsInserted(state.4.last_key_value().expect("msg").1.2.clone()),
-        // Query::DistributedRewards() => todo!(),
-        // Query::AllBets() => Reply::AllBets(state.3),
-    // };
-    // gstd::msg::reply(reply, 0).expect("Failed to share state");
+    let reply = match query {
+
+        Query::Whoami => Reply::Whoami(gstd::msg::source()), // all zero addr
+        Query::BlockNumber => Reply::BlockNumber(gstd::exec::block_height()),
+        Query::BlockTimestamp => Reply::BlockTimestamp(gstd::exec::block_timestamp()),
+        Query::ProgramId => Reply::ProgramId(gstd::exec::program_id()),
+        Query::MessageId => Reply::MessageId(gstd::msg::id()),
+        Query::All => todo!(),
+        Query::Inscribes => Reply::Inscribes(100),
+        Query::InscribesOfActorId => Reply::InscribesOfActorId(ActorId::from_bs58("16CkY8WrzVREYNSvMJKd1nLQ2S8bjGbhoYCE95thV2CqSSXX".to_owned()).expect("msg")),
+        Query::BalanceOf(_, _) => todo!(),
+    };
+    gstd::msg::reply(reply, 0).expect("Failed to share state");
 
 }
 
